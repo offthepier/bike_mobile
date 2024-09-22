afd24a6b382c60795628fd576bdc19ab06079b7f
Gayani Abeywickrama <57860284+Gayani991020@users.noreply.github.com>
2024-09-22 16:09:45 +1000

    Improve button and touchable area experience (#19)
    Co-authored-by: samin <19342925+saminadhikari@users.noreply.github.com>

--- app/lib/components/activity_buttons.dart
+++ app/lib/components/activity_buttons.dart
@@ -2,32 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:phone_app/utilities/constants.dart';
 
 class ActivityButton extends StatelessWidget {
-  // custom constructor
-  ActivityButton({required this.onTap, required this.buttonText, this.width});
-
-  // use VoidCallback instead of Function
   final VoidCallback onTap;
   final String buttonText;
   final double? width;
 
+  const ActivityButton({
+    required this.onTap,
+    required this.buttonText,
+    this.width,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          child: Text(
-            buttonText,
-            style: kBottomButtonText,
-          ),
-          alignment: Alignment.center,
-          margin: EdgeInsets.only(top: 10.0),
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          width: width ?? 180, // 180 by default if not provided
-          height: kBottomContainerHeight,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(40.0),
-            color: kHomeBtnColoursOpaQue, // Adjust border radius as needed
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(top: 10.0),
+        width: width ?? 180,
+        height: kBottomContainerHeight,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40.0),
+          color: kHomeBtnColoursOpaQue,
+        ),
+        child: InkWell(
+          onTap: () async {
+            await Future.delayed(Duration(seconds: 1));
+            onTap();
+          },
+          borderRadius: BorderRadius.circular(40.0),
+          splashColor: Colors.white.withOpacity(0.3),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 85, vertical: 16),
+            child: Text(
+              buttonText,
+              style: kBottomButtonText,
+            ),
           ),
         ),
       ),
